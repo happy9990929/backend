403d82310ade55e717c9cfe42a1bd47d6906db84
lala <lala9990929@gmail.com>
2020-08-07 16:38:55 +0800

    fontawsome add

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import Vue from 'vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -7,6 +12,9 @@ import 'jquery/dist/jquery.slim.min';
 import 'popper.js/dist/popper.min';
 import 'bootstrap/dist/js/bootstrap.min';
 import './assets/css/style.scss';
+
+library.add(fab, far, fas);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -2,7 +2,10 @@
   <div>
     <nav class="navBar">
       <ul class="pl-0">
-        <router-link tag="li" class="navItem" to="/about">關於我們</router-link>
+        <router-link tag="li" class="navItem" to="/about">
+          <font-awesome-icon :icon="['far', 'fa-user-circle']"></font-awesome-icon>
+          關於我們
+        </router-link>
         <router-link tag="li" class="navItem" to="/products">產品列表</router-link>
         <router-link tag="li" class="navItem" to="/cart">購物車</router-link>
         <router-link tag="li" class="navItem" to="/checkout">結帳</router-link>
