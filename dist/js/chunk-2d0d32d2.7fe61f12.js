bf01c4acd94df16183c8393ae775dea7dfca6663
Lala <lala@gmail.com>
2020-08-06 23:46:51 -0400

    dist
    Signed-off-by: Lala <lala@gmail.com>

---
+++ dist/js/chunk-2d0d32d2.7fe61f12.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d32d2"],{"5c3a":function(e,n,t){"use strict";t.r(n);var c=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},r=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("儀錶板")])])}],u=t("2877"),l={},a=Object(u["a"])(l,c,r,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=chunk-2d0d32d2.7fe61f12.js.map
